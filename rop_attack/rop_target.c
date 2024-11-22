59a5788c77fd368d2d76fe4c2f7c56f19b49df88
Ankit <ankithooda23@gmail.com>
2024-11-22 21:34:15 +0530

    Sample code for ROP blogpost

--- rop_attack/rop_target.c
+++ rop_attack/rop_target.c
@@ -4,6 +4,28 @@
 void function1() {
   char *addr;
   addr = (char *)0x2fc35f2d;
+
+  addr = (char *)0xc3580000;
+
+  addr = (char *)0xc3c3e18948;
+
+  addr = (char *)0xc390ca8948;
+
+  addr = (char *)0xc3d78948;
+
+  addr = (char *)0xc3d68948;
+
+  addr = (char *)0xffff050fff;
+
+  addr = (char *)0xc3909058;
+
+  addr = (char *)0xc3c20148;
+
+}
+
+long add_numbers(long a, long b) {
+  long c = a + b;
+  return c;
 }
 
 void read_from_input() {
